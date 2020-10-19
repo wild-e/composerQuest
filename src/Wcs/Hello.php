1a10bdfe68d1bcc8abd3b789aa6f98bda02251e0
wild-e <wild.emmanuelle@gmail.com>
2020-10-19 12:12:08 +0200

    finished this quest

---
+++ src/Wcs/Hello.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Wcs;
+/**
+ * Class Hello
+ *  *
+ * @package App\Wcs
+ */
+class Hello
+{
+    public function talk(): string
+    {
+        return "Hello World!";
+    }
+
+}
